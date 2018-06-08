238053f79501b559d492d2739bdc5bc3be4ffa34
dveraull <dianic.veul@gmail.com>
2018-06-08 17:28:29 -0500

    Initial Commit

---
+++ NiceButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NiceButton
+//
+//  Created by Aceleradora Mobile Perú on 8/06/18.
+//  Copyright © 2018 Aceleradora Mobile Perú. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
